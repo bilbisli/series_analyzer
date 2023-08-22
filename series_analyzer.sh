beed16360d3c67bb5caf8bc4445adea636532a12
Jonathan-fadida~ <fadida.y7@gmail.com>
2023-08-22 18:06:06 +0300

    fixed again

--- series_analyzer.sh
+++ series_analyzer.sh
@@ -23,20 +23,6 @@ validate_series(){
 	    
 }
 
-function get_array()
-{
-	local -n arr="$1"
-	local input_arr=()
-	local ret_status=0
-	
-	if [[ "${#arr[@]}" -eq 0 ]]; then
-		read -p "Enter a series (atleast 3 positive numbers separated by spaces): " input_arr
-		arr+=${input_arr}
-	fi
-		
-	
-	return $ret_status
-}
 
 --------------------------------------------------------------
 sort() {
